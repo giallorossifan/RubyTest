bd4c7068d60d04274c461abe91981ca05e2c039a
Claudio Salas <djsalas2133@gmail.com>
2022-12-08 23:09:55 -0800

    added about.html.erb

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def about
+    @title = 'About US mutha...';
+    @content =' this is the content on this page';
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # routes for landing page setup is => root 'controller#view'
   root 'posts#index'
+  get 'about' => 'pages#about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :posts
 
 end
