b3b77bcd6e68b3750fd6c7cb860ecb0fa40fedaf
Claudio Salas <djsalas2133@gmail.com>
2022-12-09 00:26:03 -0800

    added navbar etc...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # routes for landing page setup is => root 'controller#view'
-  root 'posts#index'
-  get 'about' => 'pages#about'
+  root 'posts#index', as: 'home'
+  get 'about' => 'pages#about', as: 'about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
