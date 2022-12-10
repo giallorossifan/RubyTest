924d07b84c3b5350c13f630e15c47abdee83cc34
Claudio Salas <djsalas2133@gmail.com>
2022-12-09 17:25:16 -0800

    completed blog and Comments Test App

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about', as: 'about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+        resources :comments
+  end
+
 
 end
