e29555506a5a4ef41e289c9d24769767f65567a9
Claudio Salas <djsalas2133@gmail.com>
2022-12-19 23:17:29 -0800

    put css and images

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+
+# Routes for the landing page
+root to: 'pages#home', as: :home
+get '/about', to:'pages#about', as: :about
+get '/contact', to:'pages#contact', as: :contact
+
+
+
+
 end
