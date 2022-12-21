61a2040105ae2226509662f22e513ab8826d5667
Claudio Salas <djsalas2133@gmail.com>
2022-12-20 23:32:34 -0800

    bad files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,14 @@
 class PagesController < ApplicationController
+
+  # action for the landing page
+  def home;
+  end
+
+  #action for the about page
+  def about;
+  end
+
+  #action for the contact page
+  def contact;
+  end
 end
