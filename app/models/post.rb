8388adc491408af7422cc76fb9aa1ba7306f9ad8
Claudio Salas <djsalas2133@gmail.com>
2023-01-23 16:31:54 -0800

    added posts scaffold

---
+++ app/models/post.rb
@@ -0,0 +1,31 @@
+class Post < ApplicationRecord
+
+  #validate title and body
+  validates :title, presence: true
+  #validate title has length of specific characters
+  validates :title, length: { minimum: 5 }
+  validates :title, length: { maximum: 10 }
+
+  # Custom validator to make sure there is at least one Capitol Letter
+  validate :title_must_be_at_least_one_cap
+
+  validates :body, presence: true
+
+  #validate the Uniqueness of teh title and body
+  validates :title, uniqueness: true
+  validates :body, uniqueness: true
+
+
+
+
+  private
+
+  def title_must_be_at_least_one_cap
+      return if title == title.upcase_first
+        errors.add(:title, "first leter must be capitol letter or a number")
+  end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
@@ -8,6 +9,18 @@ Rails.application.routes.draw do
 root to: 'pages#home', as: :home
 get '/about', to:'pages#about', as: :about
 get '/contact', to:'pages#contact', as: :contact
+
+
+
+get '/posts/index'
+
+
+
+
+
+
+
+
 
 
 
